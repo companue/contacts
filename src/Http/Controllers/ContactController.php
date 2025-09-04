1ae376bb84e1e0e41b55e9d34042d982566d1c65
mimalefdal <mimalefdal@yahoo.com>
2025-09-04 17:22:48 +0330

    Created ContactController class

---
+++ src/Http/Controllers/ContactController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Companue\Contacts\Http\Controllers;
+
+use Companue\Contacts\Http\Requests\ContactStoreRequest;
+use Companue\Contacts\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Companue\Contacts\Http\Resources\ContactDisplayItem;
+use Companue\Contacts\Http\Resources\ContactItem;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Response;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::all();
+        return Response::json([
+            'contacts' => ContactDisplayItem::collection($contacts)
+        ]);
+    }
+
+    public function show($id)
+    {
+        $contact = Contact::findOrFail($id);
+        return Response::json([
+            'contact' => new ContactDisplayItem($contact)
+        ]);
+    }
+
+    public function store(ContactStoreRequest $request)
+    {
+        $contact = Contact::create($request->all());
+        return Response::json([
+            'message' => Lang::get('messages.recordـcreated_with_types', ['title' => $contact->id, 'type' => 'مخاطب', 'titletype' => 'شماره']),
+            'contact' => new ContactItem($contact)
+        ]);
+    }
+
+    public function update(ContactStoreRequest $request, $id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+        return Response::json([
+            'message' => Lang::get('messages.record.updated_with_types', ['title' => $contact->id, 'type' => 'مخاطب', 'titletype' => 'شماره']),
+            'contact' => new ContactItem($contact)
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+        return Response::json([
+            'message' => Lang::get('messages.recordـdeleted_with_type', ['title' => $contact->id, 'type' => 'مخاطب']),
+        ]);
+    }
+}
