25c9061d4b0fb45d58e7e3ba4a1533285ba7a72c
mimalefdal <mimalefdal@yahoo.com>
2025-09-04 17:37:21 +0330

    Added api routes for contact

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,14 @@
+
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Companue\Contacts\Http\Controllers\ContactController;
+
+Route::prefix('contact')->group(function () {
+    Route::get('/', [ContactController::class, 'index']);
+    Route::get('{id}', [ContactController::class, 'show']);
+    Route::post('/', [ContactController::class, 'store']);
+    Route::put('{id}', [ContactController::class, 'update']);
+    Route::delete('{id}', [ContactController::class, 'destroy']);
+});
