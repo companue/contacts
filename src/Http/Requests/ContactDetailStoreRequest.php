186678942146a1c238e7a6f2aebcbcf060833098
mimalefdal <mimalefdal@yahoo.com>
2025-09-10 19:30:09 +0330

    Improve validation rules for StoreRequests

--- src/Http/Requests/ContactDetailStoreRequest.php
+++ src/Http/Requests/ContactDetailStoreRequest.php
@@ -15,11 +15,11 @@ class ContactDetailStoreRequest extends FormRequest
     {
         return [
             'contact_id'    => 'required|integer|exists:contacts,id',
-            'detail_title'  => 'required|string|max:255',
-            'address'       => 'required|string|max:255',
-            'postal_code'   => 'nullable|string|max:20',
-            'phone_number'  => 'nullable|string|max:20',
-            'mobile_number' => 'nullable|string|max:20',
+            'detail_title'  => 'nullable|string|max:255',
+            'address'       => 'required|string',
+            'postal_code'   => ['nullable', 'digits:10'],
+            'phone_number'  => ['nullable', 'regex:/^(\d{8}|0\d{10})$/'],
+            'mobile_number' => ['nullable', 'regex:/^0\d{10}$/'],
             'is_primary'    => 'boolean',
         ];
     }
--- src/Http/Requests/ContactStoreRequest.php
+++ src/Http/Requests/ContactStoreRequest.php
@@ -14,22 +14,22 @@ class ContactStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'label' => 'required|string|max:255',
+            'label' => 'required_without:name_firstname|string|max:255',
+            'name_firstname' => 'required_without:label|string|max:255',
             'type' => 'required|string|max:50',
-            'category' => 'required|string|max:50',
+            'category' => 'nullable|string|max:50',
             'title' => 'nullable|string|max:255',
-            'name_firstname' => 'nullable|string|max:255',
             'brand_lastname' => 'nullable|string|max:255',
-            'national_code' => 'nullable|string|max:20|unique:contacts,national_code',
+            'national_code' => 'nullable|digits:10|unique:contacts,national_code',
             'creator_id' => 'nullable|integer',
 
             // Accept an array of contact_details
             'contact_details' => 'array',
-            'contact_details.*.detail_title' => 'required|string|max:255',
-            'contact_details.*.address' => 'required|string|max:255',
-            'contact_details.*.postal_code' => 'nullable|string|max:20',
-            'contact_details.*.phone_number' => 'nullable|string|max:20',
-            'contact_details.*.mobile_number' => 'nullable|string|max:20',
+            'contact_details.*.detail_title' => 'nullable|string|max:255',
+            'contact_details.*.address' => 'required|string',
+            'contact_details.*.postal_code' => ['nullable', 'digits:10'],
+            'contact_details.*.phone_number' => ['nullable', 'regex:/^(\d{8}|0\d{10})$/'],
+            'contact_details.*.mobile_number' => ['nullable', 'regex:/^0\d{10}$/'],
             'contact_details.*.is_primary' => 'boolean',
         ];
     }
